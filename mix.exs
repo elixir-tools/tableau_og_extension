9ca6e89e6b3d1301c976e956e01d10ca7fb8f3ed
Mitchell Hanberg <mitch@mitchellhanberg.com>
2025-06-21 08:48:58 -0400

    refactor!: support tableau v0.25+
    BREAKING CHANGE: Only supports >= v0.25.0 of Tableau

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule TableauOgExtension.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:tableau, "~> 0.18"},
+      {:tableau, "~> 0.25.0"},
       {:nodejs, "~> 3.1"},
       {:ex_doc, ">= 0.0.0", only: :dev}
     ]
